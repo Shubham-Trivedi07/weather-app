f9ee19198b4f28e4bea29493846cacf1d2306c2e
Shubham-Trivedi07 <nirvanashubham.07@gmail.com>
2022-07-08 13:46:55 +0530

    commit

--- connect.js
+++ connect.js
@@ -2,7 +2,7 @@ const {mongoClient} = require('mongodb');
 const connFun = async() => {
     const url = 'mongodb';  // localhost:3000
     const client = new mongoClient(url);
-    const dbname = '' //name of the database
+    const dbname = 'Weatherforecast' //name of the database
     await client.connect();
     console.log('connected succesfully to the server');
     const db = client.db(dbname);
